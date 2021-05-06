a9dc05bc9c063a32fc8f8dea2e30f96f9974d252
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-07 04:22:44 +0900

    BOJ_PS BOJ_1003 : 피보나치 함수 BOJ_11279 : 최대 힙

---
+++ BOJ_1003.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int DP0[41];
+int DP1[41];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	DP0[0] = 1;
+	DP1[1] = 1;
+	int n;
+	cin >> n;
+	for (int i = 2; i <= 40; i++) {
+		DP0[i] = DP0[i - 1] + DP0[i - 2];
+		DP1[i] = DP1[i - 1] + DP1[i - 2];
+	}
+	for (int i = 0; i < n; i++) {
+		int input;
+		cin >> input;
+		cout << DP0[input] << " " << DP1[input] << endl;
+	}
+}
---
+++ BOJ_11279.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	priority_queue<int> pq;
+	int n;
+	cin >> n;
+	while (n--) {
+		int input;
+		cin >> input;
+		if (input == 0) {
+			if (pq.empty()) {
+				cout << "0" << endl;
+			}
+			else {
+				cout << pq.top() << endl;
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(input);
+		}
+	}
+}

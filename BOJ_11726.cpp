350e447e71ccdcf42d4ca5da0c57d4d4cdef40bd
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-06 16:55:47 +0900

    BOJ_11726 : 2*n 타일링 BOJ_1920 : 수 찾기

---
+++ BOJ_11726.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+#define MAX 1001
+using namespace std;
+
+int DP[MAX];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	DP[1] = 1;
+	DP[2] = 2;
+	int n;
+	cin >> n;
+	for (int i = 3; i <= n; i++) {
+		DP[i] = DP[i - 1] + DP[i - 2];
+		DP[i] %= 10007;
+	}
+	cout << DP[n];
+}
---
+++ BOJ_1920.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+	int m;
+	cin >> m;
+	sort(v.begin(), v.end());
+	for (int i = 0; i < m; i++) {
+		int input;
+		cin >> input;
+		if (binary_search(v.begin(), v.end(), input)) {
+			cout << "1" << endl;	
+		}
+		else {
+			cout << "0" << endl;
+		}
+	}
+}

e0efe2d5d69d27040daa8ad6956eda7a2e64ec24
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-08 12:14:11 +0900

    BOJ_1904 : 01타일

---
+++ BOJ_1904.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int DP[1000001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	DP[1] = 1;
+	DP[2] = 2;
+	int n;
+	cin >> n;
+	for (int i = 3; i <= n; i++) {
+		DP[i] = DP[i - 1] + DP[i - 2];
+		DP[i] %= 15746;
+	}
+	cout << DP[n];
+}

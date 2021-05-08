08234a040598e73e27888b5c31a8117b4abb45be
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-08 14:01:47 +0900

    BOJ_9461 : 파도반수열

---
+++ BOJ_9461.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+long long int DP[101];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int testcase;
+	cin >> testcase;
+	DP[1] = DP[2] = DP[3] = 1;
+	for (int i = 4; i <= 100; i++) {
+		DP[i] = DP[i - 2] + DP[i - 3];
+	}
+	for (int i = 0; i < testcase; i++) {
+		int n;
+		cin >> n;
+		cout << DP[n] << endl;
+	}
+}

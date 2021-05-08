66f86d02ed8f74802e71c4c65bdc3bc4a8a36907
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-09 08:20:24 +0900

    BOJ_1932 : 정수 삼각형

---
+++ BOJ_1932.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+// 정수 삼각형
+int n;
+int DP[501][501];
+int solution(int i, int j, int** arr) {
+	if (i == n) {
+		return 0;
+	}
+	int& ret = DP[i][j];
+	if (ret != 0) {
+		return ret;
+	}
+	return ret = max(solution(i + 1, j, arr), solution(i + 1, j + 1, arr)) + arr[i][j];
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+	int** arr = new int* [n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = new int[n];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	cout << solution(0, 0, arr);
+	for (int i = 0; i < n; i++) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+
+}

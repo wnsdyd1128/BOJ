f813693e559136081400916d13f5e69a8012f4e9
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-10 15:44:01 +0900

    BOJ_10844 : 쉬운 계단 수

---
+++ BOJ_10844.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int n;
+int DP[101][10];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+	for (int i = 1; i < 10; i++) {
+		DP[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j == 0) {
+				DP[i][j] = DP[i - 1][j + 1] % 1000000000;
+			}
+			else if(1 <= j && j < 9){
+				DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1000000000;
+			}
+			else {
+				DP[i][j] = DP[i - 1][j - 1] % 1000000000;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < 10; i++) {
+		ans += DP[n][i];
+	}
+	cout << ans % 1000000000;
+}

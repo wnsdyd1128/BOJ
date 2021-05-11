4115af42be5830fc1e8b2826096f62fc174dbae3
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-11 15:04:01 +0900

    BOJ_2164 : 카드2

---
+++ BOJ_2164.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+#define INF 1000000000
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+	while (q.size() != 1) {
+		q.pop();
+		int temp = q.front();
+		q.pop();
+		q.push(temp);
+	}
+	cout << q.front() << endl;
+}

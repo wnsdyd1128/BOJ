cc6054f48557ed916198d99387a93b9b7623132e
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-11 20:00:17 +0900

    BOJ_2748 : 피보나치 수 2

---
+++ BOJ_2748.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+#define INF 1000000000
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+ll DP[91];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	DP[0] = 0;
+	DP[1] = 1;
+	int n;
+	cin >> n;
+	for(int i = 2; i <= n; i++){
+		DP[i] = DP[i - 1] + DP[i - 2];
+	}
+	cout << DP[n] << endl;
+}

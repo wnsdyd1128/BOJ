a10f2a87f7c3edeee97b188bacb952aeb7d756ea
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-12 16:01:27 +0900

    BOJ_9095 : 1, 2, 3 더하기

---
+++ BOJ_9095.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int DP[12];
+int solution(int n) {
+	if (n == 1 || n == 2)
+		return n;
+	if (n == 3)
+		return 4;
+	int& ret = DP[n];
+	if (ret != 0)
+		return ret;
+	return ret = solution(n - 1) + solution(n - 2) + solution(n - 3);
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int TC;
+	cin >> TC;
+	while (TC--) {
+		int n;
+		cin >> n;
+		cout << solution(n) << endl;
+	}
+}

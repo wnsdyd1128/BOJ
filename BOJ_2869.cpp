46461ea31a8d6ef89868f53d6a9e247cd20d83e5
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-13 09:13:22 +0900

    BOJ_2869 : 달팽이는 올라가고 싶다

---
+++ BOJ_2869.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#define endl '\n'
+#define INF 1000000000
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int a, b, v;
+	cin >> a >> b >> v;
+	int cur = 0;
+	int days = a - b;
+	int ans = v - b - 1 / days + 1;
+
+	cout << ans << endl;
+}

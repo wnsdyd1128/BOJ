6ee7f9645e242aa6c355fdcd006fa1b5207af3d2
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-13 12:44:26 +0900

    BOJ_1427 : 소트 인 사이드

---
+++ BOJ_1427.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#define endl '\n'
+#define INF 1000000000
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	cin >> str;
+	vi sorted;
+	int length = str.length();
+	for (int i = 0; i < length; i++) {
+		sorted.push_back(static_cast<int>(str.at(i) - '0'));
+	}
+	sort(sorted.begin(), sorted.end(), greater<>());
+	int size = sorted.size();
+	for (int i = 0; i < size; i++)
+		cout << sorted.at(i);
+}

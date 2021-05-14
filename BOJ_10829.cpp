add6c7b7789c648881126d2b191bb8b5bf1aed6e
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-14 15:02:26 +0900

    BOJ_10829 : 2진수 변환

---
+++ BOJ_10829.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#define endl '\n'
+#define INF 1000000000
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+
+void toBinary(long long int num) {
+	if (num == 0)
+		return;
+	toBinary(num / 2);
+	cout << num % 2;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	long long int n;
+	cin >> n;
+	toBinary(n);
+}

41b2a70a0b57acd1443c5f788fb6be5f6f512dac
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-17 14:52:45 +0900

    BOJ_11050 : 이항 계수 1

---
+++ BOJ_11050.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int solution(int n, int k) {
+	if (n == k || k == 0)
+		return 1;
+	return solution(n - 1, k - 1) + solution(n - 1, k);
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	cout << solution(n, k) << endl;
+}

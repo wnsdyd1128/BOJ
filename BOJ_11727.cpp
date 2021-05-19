a78034a7c2bd71134aeaf377c8cc9d24a8cec529
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-19 12:04:18 +0900

    BOJ_11727 : 2*n 타일링 2

---
+++ BOJ_11727.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+ll DP[1001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	DP[1] = 1;
+	DP[2] = 3;
+	for (int i = 3; i <= n; i++) {
+		DP[i] = (DP[i - 1] + 2 * DP[i - 2]) % 10007;
+	}
+	cout << DP[n] << endl;
+
+}

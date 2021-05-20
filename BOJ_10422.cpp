ecc876e5a8c29d14b11de8bb27d3f858aa0d397f
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-20 20:55:58 +0900

    BOJ_10422 : 괄호

---
+++ BOJ_10422.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define MOD 1000000007
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+
+//Catalan Number
+//DP[n] = 괄호 n쌍을 사용했을 때의 VPS 개수 
+ll DP[5001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int TC;
+	cin >> TC;
+	DP[0] = DP[1] = 1;
+	while (TC--) {
+		int n;
+		cin >> n;
+		if (n % 2 == 1)
+			cout << "0" << endl;
+		else {
+			n /= 2;
+			for (int i = 2; i <= n; i++) {
+				DP[i] = 0;
+				for (int j = 0; j < i; j++) {
+					// BOJ_10430 참고
+					DP[i] += (((DP[j] % MOD) * (DP[i - j - 1] % MOD)) % MOD);
+				}
+			}
+			cout << DP[n] % MOD << endl;
+		}
+	}
+}

d5fabb473cae4049d6ac656d8724a31c38281664
wnsdyd1128 <mcleroysane19@inha.edu>
2021-05-24 12:43:08 +0900

    BOJ_3036 : 링

---
+++ BOJ_3036.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <numeric>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	vi list;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int input;
+		cin >> input;
+		list.push_back(input);
+	}
+	for (int i = 1; i < n; i++) {
+		int g = gcd(list.at(0), list.at(i));
+		cout << list.at(0) / g << "/" << list.at(i) / g << endl;	
+	}
+}

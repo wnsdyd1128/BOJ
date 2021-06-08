f49b6ba29e89e451423eeca97c105065cdb4f5ef
wnsdyd1128 <mcleroysane19@inha.edu>
2021-06-08 16:26:33 +0900

    BOJ_2475 : 검증수

---
+++ BOJ_2475.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int arr[5];
+	for (int i = 0; i < 5; i++) {
+		cin >> arr[i];
+	}
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		sum += arr[i] * arr[i];
+	}
+	cout << sum % 10<< endl;
+}

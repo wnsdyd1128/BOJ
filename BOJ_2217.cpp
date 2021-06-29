a3d07ae9f4820e6039dc349a01c68e3d0b67ec8a
wnsdyd1128 <mcleroysane19@inha.edu>
2021-06-29 14:56:26 +0900

    BOJ_2217 : 로프

---
+++ BOJ_2217.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int TC;
+	cin >> TC;
+	vector<ll> v;
+	while (TC--) {
+		int rope;
+		cin >> rope;
+		v.push_back(rope);
+	}
+	sort(v.begin(), v.end(), greater<int>());
+	int size = v.size();
+	ll ans = -1;
+	for (ll i = 0; i < size; i++) {
+		ans = max(ans, v.at(i) * (i + 1));
+	}
+	cout << ans;
+}

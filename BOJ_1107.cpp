3fb405c6bd8a327e7271c3a7dc6771d64c750dcf
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-05 16:04:40 +0900

    BOJ_1107 : 리모컨

---
+++ BOJ_1107.cpp
@@ -0,0 +1,55 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+bool brokenButton[10];
+int check(int num) {
+	if (!num) {
+		return brokenButton[num] ? 0 : 1;
+	}
+	int len = 0;
+	while (num > 0) {
+		if (brokenButton[num % 10])
+			return 0;
+		len++;
+		num /= 10;
+	}
+	return len;
+}
+int check(string str) {
+	for (int i = 0; i < str.length(); i++) {
+		if (brokenButton[str.at(i)])
+			return 0;
+	}
+	return str.length();
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	memset(brokenButton, false, sizeof brokenButton);
+	int N, M;
+	cin >> N >> M;
+	while (M--) {
+		int input;
+		cin >> input;
+		brokenButton[input] = true;
+	}
+	int ans = abs(N - 100);
+	for (int i = 0; i <= 1000000; i++) {
+		int newChannel = i;
+		int len = check(newChannel);
+		if (len > 0) {
+			int cnt = abs(newChannel - N);
+			ans = min(ans, len + cnt);
+		}
+	}
+	cout << ans << endl;
+}

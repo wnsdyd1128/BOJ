f0787fd1d749ca58708d5799640764312a0a998a
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-05 18:27:00 +0900

    BOJ_6064 : 카잉 달력

---
+++ BOJ_6064.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int TC;
+	cin >> TC;
+	while (TC--) {
+		int M, N, x, y;
+		cin >> M >> N >> x >> y;
+		x--;
+		y--;
+		bool flag = false;
+		for (int k = x; k < M * N; k += M) {
+			if (k % N == y) {
+				cout << k + 1 << endl;
+				flag = true;
+			}
+		}
+		if (!flag)
+			cout << -1 << endl;
+	}
+}

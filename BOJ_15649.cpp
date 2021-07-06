b6eeb58a7c70d455821b407a1b803f382b8c06bd
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-06 09:25:33 +0900

    BOJ_15649 : N과 M (1)

---
+++ BOJ_15649.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+bool isSelected[10];
+int arr[10];
+
+void Solution(int idx, int n, int m) {
+	if (idx == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << endl;
+		}
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (isSelected[i])
+			continue;
+		isSelected[i] = true;
+		arr[idx] = i;
+		Solution(idx + 1, n, m);
+		isSelected[i] = false;
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	Solution(0, n, m);
+}

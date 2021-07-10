59e34c6bc783c93226c2c815d910a6b14a1eecf1
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-10 19:12:27 +0900

    BOJ_10974 : 모든 순열

---
+++ BOJ_10974.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	vi v;
+	for (int i = 1; i <= N; i++) {
+		v.push_back(i);
+	}
+	do {
+		for (int i = 0; i < N; i++) {
+			cout << v.at(i) << ' ';
+		}
+		cout << endl;
+	} while (next_permutation(v.begin(), v.end()));
+}

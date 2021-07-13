cdb69593b258d619934f8e9db7f3f99e38b5cd9b
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-14 00:07:08 +0900

    BOJ_1106 : 호텔

---
+++ BOJ_1106(US).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstring>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int dp[21][1001];
+int C, N;
+
+int solution(int idx, int sum) {
+
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> C >> N;
+}

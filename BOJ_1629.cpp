c56f3ca6a9b4471c1fe044985391cf648a8cc2b6
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-14 20:03:25 +0900

    BOJ_1629 : 곱셈

---
+++ BOJ_1629.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int A, C;
+ll solution(int num) {
+	if (num == 0)
+		return 1;
+	if (num % 2 == 1)
+		return (solution(num - 1) * A) % C;
+	ll half = solution(num / 2);
+	return (half * half) % C;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int B;
+	cin >> A >> B >> C;
+	A %= C;
+	cout << solution(B) % C << endl;
+}

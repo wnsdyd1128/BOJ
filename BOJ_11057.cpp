78c938021425b5e6b63e8a9165edc807a0cb9505
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-17 16:05:32 +0900

    BOJ_11057 : 오르막 수

---
+++ BOJ_11057.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define MAX_N 1000
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int dp[MAX_N + 1][10];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	for (int i = 0; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < 10; j++) {
+			for (int k = 0; k <= j; k++) {
+				dp[i][j] += dp[i - 1][k] % 10007;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < 10; i++) {
+		ans += dp[N][i] % 10007;
+	}
+	cout << ans % 10007 << endl;
+
+}

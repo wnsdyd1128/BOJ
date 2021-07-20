8ea8549e153097cff2c7f7e91067b9f1fdf56207
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-20 16:41:05 +0900

    BOJ_15988 : 1, 2, 3 더하기 3

---
+++ BOJ_15988.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define MOD 1000000009
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+ll dp[1000001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int TC;
+	cin >> TC;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+	while (TC--) {
+		int n;
+		cin >> n;
+		if (dp[n] != 0) {
+			cout << dp[n] << endl;
+		}
+		else {
+			for (int i = 4; i <= n; i++) {
+				dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+				dp[i] %= MOD;
+			}
+			cout << dp[n] << endl;
+		}
+	}
+}

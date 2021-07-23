4ba450212ce6b4ff39329f2f484c757c71eaa369
wnsdyd1128 <mcleroysane19@inha.edu>
2021-07-23 21:33:15 +0900

    BOJ_10867 : 중복 빼고 정렬하기

---
+++ BOJ_10867.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int arr[2001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	while (N--) {
+		int n;
+		cin >> n;
+		arr[n + 1000]++;
+	}
+	for (int i = 0; i < 2001; i++) {
+		if (arr[i] != 0)
+			cout << i - 1000 << ' ';
+	}
+
+}

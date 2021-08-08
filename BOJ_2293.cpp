21432b55fab06d30fd80ec93fbfdada38af5b80e
wnsdyd1128 <mcleroysane19@inha.edu>
2021-08-08 16:36:04 +0900

    BOJ_2293 : 동전 1

--- BOJ_2293.cpp
+++ BOJ_2293.cpp
@@ -1,51 +1,35 @@
-﻿#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <vector>
-#include <cstring>
+﻿#include <bits/stdc++.h>
 #define endl '\n'
-#define INF 1000000000
+#define MAX_V 100000
 #define _CRT_SECURE_NO_DEPRECATE
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef vector<int> vi;
-int DP[101][10001], n, k;
 // index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
 // index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
 
-// i번째 이후 j만큼 남은 가치에 대한 DP[i][j]
-int solution(int i, int j, int* coin) {
-	if (j == 0)
-		return 1;
-	if (i == n)
-		return 0;
-	int& ret = DP[i][j];
-	if (ret != 0)
-		return ret;
-	if (j < coin[i]) {
-		ret = solution(i + 1, j, coin);
-	}
-	ret += solution(i + 1, j, coin);
-	ret += solution(i + 1, j - coin[i], coin);
-	return ret;
-}
+int dp[MAX_V + 1];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	cin >> n >> k;
-	int* arr = new int[n + 1];
-	for (int i = 1; i <= n; i++) {
-		cin >> arr[i];
+	int N, K;
+	cin >> N >> K;
+	vi v;
+	while (N--) {
+		int input;
+		cin >> input;
+		v.push_back(input);
 	}
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		ans += solution(i, k, arr);
+	dp[0] = 1;
+	for (int i = 0; i < v.size(); i++) {
+		for (int j = 1; j <= K; j++) {
+			if (j - v.at(i) < 0)
+				continue;
+			dp[j] += dp[j - v.at(i)];
+		}
 	}
-	cout << ans << endl;
+	cout << dp[K] << endl;
 }

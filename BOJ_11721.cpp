0ece2570dc3fc5fa3ea1fd2adf0f4418e80cbf34
wnsdyd1128 <mcleroysane19@inha.edu>
2021-08-18 23:59:32 +0900

    BOJ_11721 : 열 개씩 끊어서 출력하기

---
+++ BOJ_11721.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	vector<string> v;
+	string str;
+	cin >> str;
+	for (int i = 0, j = 1; i < str.length(); i++, j++) {
+		cout << str.at(i);
+		if (j == 10) {
+			j = 0;
+			cout << endl;
+		}
+	}
+}

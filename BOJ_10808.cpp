76fb7a4376f0156e206c7ee2907fe8b08f80cce0
wnsdyd1128 <mcleroysane19@inha.edu>
2021-08-24 23:46:14 +0900

    BOJ_10808 : 알파벳 개수

---
+++ BOJ_10808.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int countAlphabet[26];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		countAlphabet[str[i] - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		cout << countAlphabet[i] << ' ';
+	}
+}

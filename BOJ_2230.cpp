924b18a324fb147598b60b86e2d887d89bcf48b1
wnsdyd1128 <mcleroysane19@inha.edu>
2021-10-01 13:03:40 +0900

    BOJ_2230 : 수 고르기

---
+++ BOJ_2230.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	vi v;
+	while (n--) {
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+	vi ans;
+	for (int i = 0; i < v.size(); i++) {
+		if (lower_bound(v.begin() + i, v.end(), v[i] + m) != v.end())
+			ans.push_back(*lower_bound(v.begin() + i, v.end(), v[i] + m) - v[i]);
+	}
+	sort(ans.begin(), ans.end());
+}

302f87d7831a1604d074e19d1ec5f64a7820c1dc
wnsdyd1128 <mcleroysane19@inha.edu>
2021-10-01 16:34:17 +0900

    BOJ_12852 : 1로 만들기 2

---
+++ BOJ_12852.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define MAX 1000000 
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int dp[MAX + 1];
+int path[MAX + 1];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	dp[1] = 0;
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + 1;
+		path[i] = i - 1;
+		if (i % 2 == 0) {
+			if (dp[i] > dp[i / 2] + 1) {
+				dp[i] = dp[i / 2] + 1;
+				path[i] = i / 2;
+			}
+		}
+		if (i % 3 == 0) {
+			if (dp[i] > dp[i / 3] + 1) {
+				dp[i] = dp[i / 3] + 1;
+				path[i] = i / 3;
+			}
+		}
+	}
+	cout << dp[n] << endl;
+	int cur = n;
+	while (cur != 1) {
+		cout << cur << ' ';
+		cur = path[cur];
+	}
+	cout << 1;
+}

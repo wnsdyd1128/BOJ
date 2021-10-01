32fdfa1b288c4ead1336be1c7cdb43b1c11d1dae
wnsdyd1128 <mcleroysane19@inha.edu>
2021-10-01 21:53:09 +0900

    BOJ_22551 : Sort 마스터 배지훈의 후계자

---
+++ BOJ_20551.cpp
@@ -0,0 +1,34 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, M;
+	cin >> N >> M;
+	vi v;
+	while (N--) {
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+	while (M--) {
+		int d;
+		cin >> d;
+		if (binary_search(v.begin(), v.end(), d)) {
+			cout << lower_bound(v.begin(), v.end(), d) - v.begin() << endl;
+		}
+		else
+			cout << -1 << endl;
+	}
+}

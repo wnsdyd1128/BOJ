8de93b5605614c616ff717f488daa2f0c1a34b24
wnsdyd1128 <mcleroysane19@inha.edu>
2022-02-13 21:23:48 +0900

    BOJ_10799 : 쇠막대기

---
+++ BOJ_10799.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	cin >> str;
+	stack<char> s;
+	int ans = 0;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == '(') {
+			s.push(str[i]);
+		}
+		else if (str[i] == ')') {
+			s.pop();
+			if (str[i - 1] == ')')
+				ans++;
+			else
+				ans += s.size();
+		}
+	}
+	cout << ans << endl;
+}

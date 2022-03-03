e6b390405070e2d98028388b8b334030fe1b09a2
wnsdyd1128 <mcleroysane19@inha.edu>
2022-03-03 18:13:20 +0900

    BOJ_1965 : 상자넣기

---
+++ BOJ_1965.cpp
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define MAX_N 1000
+#define _CRT_SECURE_NO_DEPRECATE
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int dp[MAX_N + 1];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	vi v;
+	int N;
+	cin >> N;
+	while (N--) {
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+	/*int ans = 0;
+	for (int i = 0; i < v.size(); i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (v[j] < v[i])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		ans = max(ans, dp[i]);
+	}
+	cout << ans << endl;*/
+	fill(dp, dp + v.size(), INF);
+	for (int i = 0; i < v.size(); i++) {
+		*lower_bound(dp, dp + v.size(), v[i]) = v[i];
+	}
+	cout << lower_bound(dp, dp + v.size(), INF) - dp << endl;
+}

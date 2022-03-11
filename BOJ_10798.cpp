571df7514a27ea9b05cc760885257cf76f98bba7
wnsdyd1128 <mcleroysane19@inha.edu>
2022-03-11 22:26:35 +0900

    BOJ_10798 : 세로읽기

---
+++ BOJ_10798.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	vector<string> v;
+	for (int i = 0; i < 5; i++) {
+		string str;
+		cin >> str;
+		v.push_back(str);
+	}
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < v.size(); j++) {
+			if (i < v[j].size())
+				cout << v[j][i];
+		}
+	}
+}

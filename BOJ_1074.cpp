d532c56d0a4a4604eff596d8e2372cdb9fa3aa22
wnsdyd1128 <mcleroysane19@inha.edu>
2022-03-30 23:23:31 +0900

    BOJ_1074 : Z

---
+++ BOJ_1074.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int N, r, c;
+int cnt;
+void solution(int row, int col, int scale) {
+	if (scale == 1) {
+		if (row == r && col == c) {
+			cout << cnt << endl;
+		}
+		cnt++;
+		return;
+	}
+	if (row <= r && r < row + scale && col <= c && c < col + scale) {
+		int half = scale / 2;
+		solution(row, col, half);
+		solution(row, col + half, half);
+		solution(row + half, col, half);
+		solution(row + half, col + half, half);
+	}
+	else {
+		cnt += scale * scale ;
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N >> r >> c;
+	solution(0, 0, (1 << N));
+}

f456df671b9a67aec142dce2f3d34712f4156524
wnsdyd1128 <mcleroysane19@inha.edu>
2022-04-25 21:56:03 +0900

    // Unsolved BOJ 9252

--- BOJ_9252.cpp
+++ BOJ_9252.cpp
@@ -9,32 +9,44 @@ typedef vector<ii> vii;
 typedef vector<int> vi;
 // index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
 // index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
-int dp[MAX_LEN + 1][MAX_LEN + 1];
-string str[MAX_LEN + 1][MAX_LEN + 1];
+pair<char, ii> str[MAX_LEN + 1][MAX_LEN + 1];
+int dp[MAX_LEN + 1][MAX_LEN + 1]; // dp[i][j] := 문자열 s(1~i)와 문자열 t(1~j)에 대한 LCS
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	string str1, str2;
-	cin >> str1 >> str2;
-	for (int i = 0; i < str1.length(); i++) {
-		for (int j = 0; j < str2.length(); j++) {
-			if (str1[i] == str2[j]) {
+	string s, t;
+	cin >> s >> t;
+	stack<char> ans;
+	for(int i = 0; i < s.length(); i++) {
+		for(int j = 0; j < t.length(); j++) {
+			if(s[i] == t[j]) {
 				dp[i + 1][j + 1] = dp[i][j] + 1;
-				str[i + 1][j + 1] = str[i][j] + str1.at(i);
+				str[i + 1][j + 1].second = make_pair(i, j);
 			}
 			else {
-				if (dp[i][j + 1] < dp[i + 1][j]) {
+				if(dp[i][j + 1] < dp[i + 1][j]) {
 					dp[i + 1][j + 1] = dp[i + 1][j];
-					str[i + 1][j + 1] = str[i + 1][j];
+					str[i + 1][j + 1].second = { i + 1, j };
 				}
 				else {
 					dp[i + 1][j + 1] = dp[i][j + 1];
-					str[i + 1][j + 1] = str[i][j + 1];
+					str[i + 1][j + 1].second = { i, j + 1 };
 				}
 			}
 		}
 	}
-	cout << dp[str1.length()][str2.length()] << endl;
-	cout << str[str1.length()][str2.length()] << endl;
+	cout << dp[s.length()][t.length()] << endl;
+	int row = s.length();
+	int col = t.length();
+	while(dp[row][col] != 0) {
+		if (s[row - 1] == t[col - 1])
+			ans.push(s[row - 1]);
+		row = str[row][col].second.first;
+		col = str[row][col].second.second;
+	}
+	while(!ans.empty()) {	
+		cout << ans.top();
+		ans.pop();
+	}
 }

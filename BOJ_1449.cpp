8c675528012187dc71c5daa4a9b0f8d6f80cda40
wnsdyd1128 <mcleroysane19@inha.edu>
2022-04-26 18:28:09 +0900

    BOJ_1449 : 수리공 항승

---
+++ BOJ_1449.cpp
@@ -0,0 +1,37 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int N, L;
+	cin >> N >> L;
+	vi v;
+	while(N--) {
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+	int i = 0;
+	int ans = 0;
+	while(i < v.size()) {
+		int j = i + 1;
+		while (j < v.size() && v[i] + L > v[j])
+			j++;
+		ans++;
+		i = j;
+	}
+	cout << ans << endl;
+}

63ddd0c0e087df53bb05f6bbcecf51585c12a357
wnsdyd1128 <mcleroysane19@inha.edu>
2022-05-10 16:47:44 +0900

    BOJ_10824 : 네 수

---
+++ BOJ_10824.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define _CRT_SECURE_NO_DEPRECATE
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#pragma warning(disable:4996)
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
+// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string a, b, c, d;
+	cin >> a >> b >> c >> d;
+	string lhs = a + b;
+	string rhs = c + d;
+	ll ans = stoll(lhs) + stoll(rhs);
+	cout << ans << endl;
+}

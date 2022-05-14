2f64f86079d88a1574df9772491519781d269a27
wnsdyd1128 <mcleroysane19@inha.edu>
2022-05-15 07:35:15 +0900

    BOJ_1753 : 최단경로

--- BOJ_1753.cpp
+++ BOJ_1753.cpp
@@ -11,7 +11,6 @@ typedef vector<int> vi;
 // index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
 // index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
 vii v[MAX_V + 1];
-int dist[MAX_V + 1];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -23,20 +22,19 @@ int main() {
 		cin >> from >> to >> weight;
 		v[from].push_back(make_pair(weight, to));
 	}
-	fill(dist, dist + V + 1, INF);
+	vi dist(MAX_V + 1, INF);
 	dist[st] = 0;
 	priority_queue<ii, vii, greater<>> q;
 	q.push(make_pair(0, st));
 	while(!q.empty()) {
-		int weight = q.top().first;
-		int dest = q.top().second;
+		auto [weight, dest] = q.top();
 		q.pop();
 		if (dist[dest] != weight)
 			continue;
-		for(auto nxt : v[dest]) {
-			if(dist[nxt.second] > dist[dest] + nxt.first) {
-				dist[nxt.second] = dist[dest] + nxt.first;
-				q.push(make_pair(dist[nxt.second], nxt.second));
+		for(auto [w, d] : v[dest]) {
+			if(dist[d] > dist[dest] + w) {
+				dist[d] = dist[dest] + w;
+				q.push(make_pair(dist[d],d));
 			}
 		}
 	}

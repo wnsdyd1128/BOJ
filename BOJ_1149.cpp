1aa523b2b03c31296dcbba984fadcc2f8bc6541a
wnsdyd1128 <mcleroysane19@inha.edu>
2022-05-16 18:08:02 +0900

    BOJ_1149 : RGB거리

--- BOJ_1149.cpp
+++ BOJ_1149.cpp
@@ -1,50 +1,43 @@
-﻿#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <vector>
+﻿#include <bits/stdc++.h>
 #define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define MAX_N 1000
 using namespace std;
-
-int n;
-int DP[1001][3];
-int solution(int idx, int color, vector<pair<int, pair<int, int>>> cost) {
-	if (idx == 0) {
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef tuple<int, int, int> tp;
+typedef vector<tp> vtp;
+vtp v;
+int dp[MAX_N + 1][3];
+int N;
+int solution(int idx, int color) {
+	if (idx < 0)
 		return 0;
-	}
-	int& ret = DP[idx][color];
-	if (ret != 0)
+	int& ret = dp[idx][color];
+	if (ret != -1)
 		return ret;
-	if (color == 0) {
-		// 현재의 원소를 더해주면 DP[idx][color]가 0이 될 수 없다! 
-		ret = min(solution(idx - 1, 1, cost), solution(idx - 1, 2, cost)) + cost.at(idx - 1).first;
-		return ret;
-	}
-	else if (color == 1) {
-		ret = min(solution(idx - 1, 0, cost), solution(idx - 1, 2, cost)) + cost.at(idx - 1).second.first;
-		return ret;
-	}
-	else {
-		ret = min(solution(idx - 1, 0, cost), solution(idx - 1, 1, cost)) + cost.at(idx - 1).second.second;
-		return ret;
-	}
+	auto [r, g, b] = v[idx];
+	if (color == 0)
+		ret = min(solution(idx - 1, 1), solution(idx - 1, 2)) + r;
+	else if (color == 1)
+		ret = min(solution(idx - 1, 0), solution(idx - 1, 2)) + g;
+	else
+		ret = min(solution(idx - 1, 0), solution(idx - 1, 1)) + b;
+	return ret;
 }
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	vector<pair<int, pair<int, int>>> v; // first : R, second.first : G, second.second : B
-	cin >> n;
-	for (int i = 0; i < n; i++) {
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> N;
+	v.resize(N + 1);
+	memset(dp, -1, sizeof dp);
+	for (int i = 0; i < N; i++) {
 		int r, g, b;
 		cin >> r >> g >> b;
-		v.push_back(make_pair(r, make_pair(g, b)));
+		v[i] = make_tuple(r, g, b);
 	}
-	DP[1][0] = v.at(0).first;
-	DP[1][1] = v.at(0).second.first;
-	DP[1][2] = v.at(0).second.second;
-	cout << min(solution(n, 0, v), min(solution(n, 1, v), solution(n, 2, v)));
-
+	cout << min({ solution(N - 1, 0), solution(N - 1, 1), solution(N - 1, 2) }) << endl;
 }

9d8b4edd5dedb7cf1de8b75e527df04c1437767e
wnsdyd1128 <mcleroysane19@inha.edu>
2022-05-17 22:40:50 +0900

    BOJ_11727 : 2×n 타일링 2

--- BOJ_11727.cpp
+++ BOJ_11727.cpp
@@ -1,33 +1,23 @@
-﻿#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <vector>
-#include <cstring>
+﻿#include <bits/stdc++.h>
 #define endl '\n'
 #define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
-#define _CRT_SECURE_NO_DEPRECATE
+#define MOD 10007
+#define MAX_N 1000
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef vector<int> vi;
-// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
-// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
-ll DP[1001];
+ll dp[MAX_N + 1];
+int N;
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;
-	cin >> n;
-	DP[1] = 1;
-	DP[2] = 3;
-	for (int i = 3; i <= n; i++) {
-		DP[i] = (DP[i - 1] + 2 * DP[i - 2]) % 10007;
-	}
-	cout << DP[n] << endl;
-
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> N;
+	dp[2] = 3;
+	dp[1] = 1;
+	for (int i = 3; i <= N; i++)
+		dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % MOD;
+	cout << dp[N] << endl;
 }

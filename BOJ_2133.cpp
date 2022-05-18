7bd5cc671f61a01ecc5a7bc5d24b01e3d15056b8
wnsdyd1128 <mcleroysane19@inha.edu>
2022-05-18 14:02:57 +0900

    BOJ_2133 : 타일 채우기

--- BOJ_2133.cpp
+++ BOJ_2133.cpp
@@ -1,27 +1,26 @@
 ﻿#include <bits/stdc++.h>
 #define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
 #define MAX_N 30
-#define _CRT_SECURE_NO_DEPRECATE
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef vector<int> vi;
-// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
-// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
 ll dp[MAX_N + 1];
+int N;
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;
-	cin >> n;
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 	dp[0] = 1;
-	for (int i = 2; i <= n; i++) {
+	dp[1] = 0;
+	dp[2] = 3;
+	cin >> N;
+	for (int i = 3; i <= N; i++) {
 		dp[i] = 3 * dp[i - 2];
-		for (int j = i - 4; j >= 0; j -= 2) {
+		for (int j = i - 4; j >= 0; j -= 2)
 			dp[i] += 2 * dp[j];
-		}
 	}
-	cout << dp[n] << endl;
+	cout << dp[N] << endl;
 }

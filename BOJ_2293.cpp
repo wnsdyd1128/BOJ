ffaecbbfe65e224756163e7802341a099eaffabc
wnsdyd1128 <mcleroysane19@inha.edu>
2022-05-18 14:32:02 +0900

    BOJ_2293 : 동전 1

--- BOJ_2293.cpp
+++ BOJ_2293.cpp
@@ -1,35 +1,27 @@
 ﻿#include <bits/stdc++.h>
 #define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
 #define MAX_V 100000
-#define _CRT_SECURE_NO_DEPRECATE
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 typedef vector<int> vi;
-// index = (index + 1 ) % n;		// index++; if (index >= n) index = 0;
-// index = (index + n - 1 ) % n;	// index--; if (index < 0) index = n - 1;
-
 int dp[MAX_V + 1];
+int N, K;
+vi v;
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int N, K;
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 	cin >> N >> K;
-	vi v;
-	while (N--) {
-		int input;
-		cin >> input;
-		v.push_back(input);
-	}
+	v.resize(N + 1);
+	for (int i = 0; i < N; i++)
+		cin >> v[i];
 	dp[0] = 1;
-	for (int i = 0; i < v.size(); i++) {
-		for (int j = 1; j <= K; j++) {
-			if (j - v.at(i) < 0)
-				continue;
-			dp[j] += dp[j - v.at(i)];
-		}
+	for(int i = 0; i < N; i++) {
+		for (int j = v[i]; j <= K; j++)
+			dp[j] += dp[j - v[i]];
 	}
 	cout << dp[K] << endl;
 }

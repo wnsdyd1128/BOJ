a304ca43de665f5c5f890a9cd2a27101c1ffac8d
wnsdyd1128 <mcleroysane19@inha.edu>
2022-05-18 21:16:29 +0900

    BOJ_2075: N번째 큰 수

---
+++ BOJ_2075.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+#define endl '\n'
+#define INF 1000000000 // 10^9, Floyd-Warshall에선 20억보다 안전
+#define MAX_N 1500
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+int N;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	priority_queue<int, vi, greater<>> q;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int data;
+			cin >> data;
+			q.push(data);
+		}
+		if (i != 0) {
+			for (int k = 0; k < N; k++)
+				q.pop();
+		}
+	}
+	cout << q.top() << endl;
+}

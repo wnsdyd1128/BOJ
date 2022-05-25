9930fd9ebbcbb71fb9ac34d513bea5cd13cdbaa6
wnsdyd1128 <mcleroysane19@inha.edu>
2022-05-25 20:10:52 +0900

    BOJ_14502 : 연구소

--- BOJ_14502.cpp
+++ BOJ_14502.cpp
@@ -41,7 +41,7 @@ int countSafeArea(const vector<vi>& grid) {
 	for (int i = 0; i < grid.size(); i++) {
 		for (int j = 0; j < grid[i].size(); j++) {
 			if (grid[i][j] == 0)
-				ret++;
+				ret++;	
 		}
 	}
 	return ret;
